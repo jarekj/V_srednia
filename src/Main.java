7b9465ce0a259919f59252eb725acac376bc6cf2
Jarek <jjankowski1@gmail.com>
2018-06-07 20:03:19 +0200

    main również przeszedł spoja

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 class Main {
 
     public static void main(String[] args) throws java.lang.Exception {
-        Scanner scanner = new Scanner(System.in);
-        String t = scanner.nextLine();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
+
+        String t = in.readLine();
         int tInt = Integer.parseInt(t);
-        int v1, v2;
 
         for (int i = 0; i < tInt; i++) {
-            v1 = scanner.nextInt();
-            v2 = scanner.nextInt();
-            System.out.println((2 * v1 * v2 / (v1 + v2)));
+            String input = in.readLine();
+            String[] inputTab = input.split("\\s");
+            int v1 = Integer.parseInt(inputTab[0]);
+            int v2 = Integer.parseInt(inputTab[1]);
+            writer.println((2 * v1 * v2) / (v1 + v2));
         }
+        writer.close();
     }
 }
